1ceec5d647c5bde1d51d26cbbf3d6d80b3702afd
Josh Arnold <jarnold@ucdavis.edu>
2021-05-25 16:20:13 -0400

    add in eec172 stuff

--- server.py
+++ server.py
@@ -10,5 +10,24 @@ def splash():
     return render_template('index.html')
 
 
+# ~~ EEC 172 - Final Project ~~
+
+
+global CURRENT_TEMP
+CURRENT_TEMP = -1
+
+
+@app.route('/set_temp/<temp>')
+def set_temp(temp):
+    global CURRENT_TEMP
+    CURRENT_TEMP = temp
+    return "Success!"
+
+
+@app.route('/get_temp')
+def get_temp():
+    return CURRENT_TEMP
+
+
 if __name__ == '__main__':
     app.run(port=os.getenv('PORT', 8000))
