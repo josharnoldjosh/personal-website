94cac77181e7f80a6410247e70c364b141f1bc1f
Josh Arnold <jarnold@ucdavis.edu>
2021-05-29 12:58:27 -0400

    add in eec172 stuff

--- server.py
+++ server.py
@@ -10,11 +10,11 @@ def splash():
     return render_template('index.html')
 
 
+# --- EEC 172  Below ---
+
+
 @app.route('/temp', methods=['GET', 'POST'])
 def temp():    
-    """
-    EEC 172 Final Project.
-    """
     if request.method == 'GET':
         try:
             with open('./temp.txt', 'r') as f:
@@ -25,7 +25,31 @@ def temp():
     elif request.method == 'POST':
         temp = request.form.get('temp', -1)
         with open('./temp.txt', 'w') as f:
-            f.write(f"{temp}")            
+            f.write(f"{temp}")         
+
+
+@app.route('/registerpush', methods=['POST'])
+def register_for_push_notifications():
+    token = request.form.get('token', '')
+    if token:
+        result = [token]
+        try:
+            with open('./tokens.txt', 'r') as f:
+                result += [i for i in f.readlines() if i.strip()]
+        except Exception as e:
+            print(e)
+        with open('./tokens.txt', 'w') as f:
+            f.writelines(result)
+
+
+@app.route('/tokens', methods=['GET'])
+def get_tokens():
+    try:
+        with open('./tokens.txt', 'r') as f:
+            'return \n'.join(f.readlines())
+    except Exception as e:
+        print(e)
+    return e
 
 
 if __name__ == '__main__':
